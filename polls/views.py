adae7971aa27acc7cb86ab7aed5029489f9c69bd
Pedro Azevedo <pedro-azevedo@outlook.com.br>
2022-10-05 10:32:27 -0300

    Added path access to href

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
 import http
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render
 from django.http import HttpResponse
 from .models import Question
+from django.template import loader
 
 # Create your views here.
 def index(request):
@@ -13,12 +14,19 @@ def index(request):
     }
     # Alternatively, you can use the render() method instead of
     # return HttpResponse(template.render(context, request))
-    # We can also remove the dependency from django.template import loader
+    # We can also remove the dependency
     return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s" % question_id)
+    # try:
+    #     question = Question.objects.get(pk= question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    
+    # can also be written as when adding 'from django.shortcuts import get_object_or_404'
+    question = get_object_or_404(Question, pk= question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "Your're looking at the results of question %s"
